ae3f3ba44d4271647513aa404434df5e7eda9469
DAVID-JOS <oluwayimikadavid2@gmail.com>
2025-10-01 12:03:45 +0100

    Delete server directory
    Yes

--- server/backend.js
+++
@@ -1,9 +0,0 @@
-import { Meteor } from "meteor/meteor";
-import "../imports/api/wallets.js";
-import "../imports/api/mining.js";
-import "../imports/api/exchange.js";
-import "../imports/api/transfer_api.js";
-
-Meteor.startup(() => {
-  console.log("🚀 Mine App backend started");
-});
